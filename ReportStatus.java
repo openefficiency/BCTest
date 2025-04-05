bdbf3496b743b90ac0ab3252e42b909422e171f8
openefficiency <info@openefficiency.org>
2025-04-05 10:20:31 -0700

    Create ReportStatus.java

---
+++ ReportStatus.java
@@ -0,0 +1,16 @@
+package org.whistleblower;
+import com.owlike.genson.annotation.JsonValue;
+public enum ReportStatus {
+SUBMITTED("SUBMITTED"),
+UNDER_INVESTIGATION("UNDER_INVESTIGATION"),
+PENDING_REVIEW("PENDING_REVIEW"),
+CLOSED("CLOSED");
+private final String status;
+ReportStatus(final String status) {
+this.status = status;
+}
+@JsonValue
+public String toString() {
+return this.status;
+}
+}
